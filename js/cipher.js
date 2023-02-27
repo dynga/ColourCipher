e4824c44c10ac159105e97bd7836872690a9b4e8
dynga <92644704+dynga@users.noreply.github.com>
2023-02-27 20:25:52 +0100

    Finish up encoding code

--- js/cipher.js
+++ js/cipher.js
@@ -1,14 +1,7 @@
 const epsilon = 10;
 
-/**
- * 'random' - base case, pick a hex code from the array and randomize within given epsilon
- * 'exact' - single hex code, has to be exact, don't randomize
- * 'lowereq' - single hex code, final value has to be less or equal than the template
- * 'highereq' - single hex code, final value has to be higher or equal than the template
- * @type {string[]}
- */
-
-
+const source = document.getElementById('source');
+const target = document.getElementById('target');
 
 const cipher = [
     ['a', ['#FFBF00']],
@@ -39,42 +32,101 @@ const cipher = [
     ['z', ['#39A78E']]
 ]
 
-const hexExact = ['#FFFFFF', '#EEED09', '#FFF700']
-const hexDarkerThan = ['#727472']
+function encode() {
+    let text = source.value;
+    for (let i = 0; i < cipher.length; i++) {
+        text = text.replaceAll(cipher[i][0], prepareHexCode(cipher[i][1]) + ' ')
+    }
+    target.value = text;
+}
 
-
-function prepareHexCode( hexArray ) {
-    switch( hexArray[0] ) {
-        case 'random':
-            return
+let fresh = true;
+function clearOnce() {
+    if (fresh) {
+        source.value = '';
+        source.style.color = 'black';
+        fresh = false;
+    }
+}
+function resetIfBlank() {
+    if (source.value === '') {
+        source.style.color = 'grey';
+        source.value = 'aeiouy';
+        target.style.color = 'grey';
+        target.value = 'aeioue';
+        // copyButton.style.display = "none";
+        // fresh = true;
     }
 }
 
-function randNumWithinEpsilon( epsilon ) {
-    let rand = math.floor(math.random() * 2)
-    let num = math.floor(math.random() * epsilon)
-    if (rand > 1) {
-        return num * -1;
-    }
-    else {
-        return num;
+function prepareHexCode(hexArray) {
+    let picked = pickRandomFromArray(hexArray);
+    switch(picked) {
+        //Those hex codes need to be exact. Do not randomize.
+        case '#FFFFFF':
+        case '#EEED09':
+        case '#FFF700':
+            return randomizeHexCode(picked, 0);
+        //Those hex codes can only be randomized to be darker than the original.
+        case '#727472':
+            return randomizeHexCode(picked, -1);
+        //Those hex codes can only be randomized to be lighter than the original.
+        case '#808080':
+            return randomizeHexCode(picked, 1);
+        //All other hex codes. Randomize within epsilon.
+        default:
+            return randomizeHexCode(picked, randPosOrNeg());
     }
 }
 
-function hexToDecArr( hex ) {
-    let r = parseInt(hex.slice(1,2), 16);
-    let g = parseInt(hex.slice(3,4), 16);
-    let b = parseInt(hex.slice(5,6), 16);
+function randPosOrNeg() {
+    switch (Math.floor(Math.random() * 2)) {
+        case 0:
+            return -1;
+        case 1:
+            return 1;
+    }
+}
+
+function randomizeHexCode(hexCode, coeff) {
+    return decArrToHex(randomizeDecArr(hexToDecArr(hexCode), coeff));
+}
+
+function randNumWithinEpsilon(epsilon, coeff) {
+    let num = Math.floor(Math.random() * epsilon)
+    return num * coeff;
+}
+
+function pickRandomFromArray(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+function hexToDecArr(hex) {
+    let r = parseInt(hex.slice(1,3), 16);
+    let g = parseInt(hex.slice(3,5), 16);
+    let b = parseInt(hex.slice(5), 16);
     return [r, g, b];
 }
 
-function decArrToHex ( decArr ) {
-    return '#'.concat(decArr[0].toString(16), decArr[1].toString(16), decArr[2].toString(16));
+function decArrToHex(decArr) {
+    for (let i = 0; i < decArr.length; i++) {
+        decArr[i] = decArr[i].toString(16).toUpperCase().padStart(2, '0');
+    }
+    return '#'.concat(decArr[0], decArr[1], decArr[2]);
 }
 
-function fuckYourShitUp( decArr ) { //todo: change the name for fuck's sake
-    decArr[0] = decArr[0] + randNumWithinEpsilon( epsilon );
-    decArr[1] = decArr[1] + randNumWithinEpsilon( epsilon );
-    decArr[2] = decArr[2] + randNumWithinEpsilon( epsilon );
+/**
+ * Randomizes each value in the array of color values.
+ * @param decArr Array to be randomized.
+ * @param limit Whether or not to limit the epsilon in any way.
+ *  -1 makes the result value be only less or equal than source value.
+ *   1 makes it only higher or equal.
+ *   0 turns off randomization.
+ * @returns {*}
+ */
+function randomizeDecArr(decArr, limit) {
+    decArr[0] = Math.abs(decArr[0] + randNumWithinEpsilon(epsilon, limit));
+    decArr[1] = Math.abs(decArr[1] + randNumWithinEpsilon(epsilon, limit));
+    decArr[2] = Math.abs(decArr[2] + randNumWithinEpsilon(epsilon, limit));
     return decArr;
 }
